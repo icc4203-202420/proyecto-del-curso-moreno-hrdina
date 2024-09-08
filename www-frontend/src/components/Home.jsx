3f53b70f80f69ad21b59ac90037987b84de20fb0
apps-moviles <apps-moviles@apps.myguest.virtualbox.org>
2024-09-07 23:53:53 -0400

    fixes

--- www-frontend/src/components/Home.jsx
+++ www-frontend/src/components/Home.jsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Container, Typography, Button, Card, CardContent } from '@mui/material';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Home() {
+  
   return (
     <Container>
       <Typography variant="h4" gutterBottom>
         Welcome to BeerApp!
       </Typography>
-      <Button variant="contained" color="primary" component={Link} to="/beers">
-        Beers
-      </Button>
-      <Button variant="contained" color="secondary" component={Link} to="/bars">
-        Bars
-      </Button>
-      <Button variant="contained" color="success" component={Link} to="/search-user">
-        Search User
-      </Button>
+
     </Container>
   );
 }
